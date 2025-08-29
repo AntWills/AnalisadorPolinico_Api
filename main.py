9223dd7ff47643f1fa796619d5dbff52feaa392e
Antonio Wills <antoniowillissilvasantos@gmail.com>
2025-08-29 07:06:26 -0300

    test2

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://antwills.github.io"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
